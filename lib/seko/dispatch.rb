c1d28e35187b0e03ff1f35f5c6af0db222be2611
Justin Grubbs <justin@jgrubbs.net>
2014-10-27 10:34:59 -0400

    fix broken tests

--- lib/seko/dispatch.rb
+++ lib/seko/dispatch.rb
@@ -1,12 +1,13 @@
 module Seko
   class Dispatch
 
-    def dispatches
+    def self.line_items(response)
       response.root_response["List"]["DispatchLineItem"]
     end
 
     # returns array of GUIDs
     def self.parse(response)
+      dispatches = line_items(response)
       # API is flawed and returns an array if multiple dispatches
       if dispatches.is_a?(Array)
         dispatches.collect { |h| h["GUID"] }
