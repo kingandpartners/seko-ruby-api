eab7321899c8ee1d1d17da445d44ad6bb1d10f8a
Justin Grubbs <justin@jgrubbs.net>
2014-11-14 18:15:02 -0500

    add tracking regex to figure out carrier from tracking number

--- lib/seko/tracking.rb
+++ lib/seko/tracking.rb
@@ -4,10 +4,26 @@ module Seko
     attr_accessor :carrier
 
     DPD = "http://www.dpd.co.uk/apps/tracking/?reference=:tracking_number&postcode=#results"
+    UPS = "http://wwwapps.ups.com/WebTracking/track?trackNums=:tracking_number&track.x=Track"
+
+    TRACKING_NUMBERS = {
+      'UPS' => /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/i,
+      'DPD' => /\b([0-9]{14} ?[A-Z]{1})/i
+    }
+
+    def get_carrier
+      TRACKING_NUMBERS.each do |carrier, pattern|
+        return carrier.dup if @tracking_number =~ pattern 
+      end
+      return 'DPD' # return DPD by default
+    end
 
     def initialize(carrier, tracking_number)
-      @carrier         = carrier
+      # FIXME: dependent apps currently rely on two arguments 
+      # (carrier and tracking_number) therefore we should keep 
+      # carrier, but should removed in next minor level release
       @tracking_number = tracking_number
+      @carrier         = get_carrier
     end
 
     def carrier_destination
